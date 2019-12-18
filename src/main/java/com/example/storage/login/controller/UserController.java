00ad4690d112c9d9c1a536ed9b0e4e1a744f140f
liuyang <373299139@qq.com>
2019-12-18 15:18:01 +0800

    前后分离

--- src/main/java/com/example/storage/login/controller/UserController.java
+++ src/main/java/com/example/storage/login/controller/UserController.java
@@ -2,12 +2,13 @@ package com.example.storage.login.controller;
 
 import com.example.storage.login.entityBean.User;
 import com.example.storage.login.service.UserService;
-import com.example.storage.redis.RedisUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,54 +17,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/userController")
+@RequestMapping("/rest/userController")
 @Log4j2
 public class UserController {
 
     @Autowired
     private UserService userService;
     @RequestMapping("/user_login")
-    public String user_login(User users,Model model, HttpServletRequest HttpServletRequest, HttpServletResponse HttpServletResponse){
-        log.info("登录人账号"+users.getUsername());
-        RedisUtil redisUtil = new   RedisUtil();;
-        redisUtil.set("name","liu");
-        redisUtil.get("name");
-        redisUtil.set("name","yang");
-        redisUtil.get("name");
-//(
-        if(!users.getUsername().equals("")&&!users.getUserpsw().equals("")){
-           User uuser = userService.seleUser(users);
-            if(uuser!=null){
-                HttpSession session = HttpServletRequest.getSession();
-                session.setAttribute("user",uuser);
-                List li = new ArrayList();
-                li.add(uuser);
-                model.addAttribute("users",li);
-                return "home/home";
-            }else{
-                return "index";
+    @ResponseBody
+    public Boolean user_login(@RequestBody User users, HttpServletRequest HttpServletRequest, HttpServletResponse HttpServletResponse) {
+        System.out.println("sdsd");
+        if (users != null) {
+            if (!users.getUsername().equals("") && !users.getUserpsw().equals("")) {
+                User uuser = userService.seleUser(users);
+                if (uuser != null) {
+                    HttpSession session = HttpServletRequest.getSession();
+                    session.setAttribute("user", uuser);
+                    List li = new ArrayList();
+                    li.add(uuser);
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                String msg = "请正确输入账号密码";
+                return false;
             }
-        }else{
-            String msg = "请正确输入账号密码";
-            model.addAttribute("msg",msg);
-            return "index";
         }
-//        User user = new User();
-//        user.setId(1);
-//        user.setUsername("123");
-//        user.setUserpsw("123");
-//        user.setUserphone(123);
-//        user.setPopedom(0);
-//        HttpSession session =  HttpServletRequest.getSession();
-//        session.setAttribute("user",user);
-//        log.info("创建一个对象");
-//        List list = new ArrayList();
-//        list.add(user);
-//        model.addAttribute("users",list);
-//        log.info("出去方法");
+        return false;
     }
     @RequestMapping("/a")
     public String a(){
         return "index";
+    }
+    @RequestMapping("/b")
+    public String b(){
+        return "home/adminHome";
     }
 }
